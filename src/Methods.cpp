be4c5144c2a9e3e8dacc9aefd38d2bdd27c97a24
Ignacio Tarrio <itarrio@despegar.com>
2017-10-01 19:40:57 -0300

    agregar el guess al power method

--- src/Methods.cpp
+++ src/Methods.cpp
@@ -1,9 +1,13 @@
+#include <cstdlib>
 #include "Methods.h"
 #include "matrix/matrix.h"
 
 namespace Methods {
 
     double power_method(matrix<double> &B, row<double> &v, double delta, unsigned long iterations) {
+        for (size_t vi = 0; vi < v.size(); ++vi){
+            v[vi] = (double)rand(); //TODO: ver el rango de los numeros aleatorios
+        }
         double old_lambda = INFINITY;
         row<double> bv = Matrix::dotProductWithVector(B, v);
         for (unsigned long i = 0; i < iterations; i++) {
