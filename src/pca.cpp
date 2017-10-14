76ca4c1fc4f3f5d030eddfe49c35469d5320931a
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2017-10-14 19:51:18 -0300

    Separar PCA del header

---
+++ src/pca.cpp
@@ -0,0 +1,64 @@
+#include <limits>
+#include "power.h"
+#include "benchmark/benchmark.h"
+#include "pca.h"
+
+#define BENCH_FILE_EIGENVECTORS "bench-eigenvectors.csv"
+
+vector<double> get_mean(const Matrix<double> &X) {
+    vector<double> mean;
+    for (size_t j = 0; j < X.width(); j++) {
+        double sum = 0;
+        for (size_t i = 0; i < X.height(); i++)
+            sum += X[i][j];
+        mean.push_back(sum / X.height());
+    }
+    return mean;
+}
+
+MatrixRef<double> substract_mean(const Matrix<double>& X, vector<double> &mean) {
+    MatrixRef<double> result = X.makeNew();
+    for (size_t i = 0; i < X.height(); i++) {
+        for (size_t j = 0; j < X.width(); j++) {
+            (*result)[i][j] = X[i][j] - mean[j];
+        }
+    }
+    return result;
+}
+
+vector<vector<double>> get_eigenvectors(MatrixRef<double> M_x, unsigned int alfa) {
+    vector<vector<double>> eigenvectors;
+    INIT_BENCH(BENCH_FILE_EIGENVECTORS);
+    for (unsigned int i = 0; i < alfa; i++) {
+        vector<double> eigenvector;
+        START_BENCH;
+        double eigenvalue = power_method(M_x, eigenvector);
+        END_BENCH(BENCH_FILE_EIGENVECTORS);
+        eigenvectors.push_back(eigenvector);
+        M_x = deflate(M_x, eigenvector, eigenvalue);
+    }
+    return eigenvectors;
+}
+
+/*
+ * 1 <= alfa <= filas(X)
+ */
+MatrixRef<double> pca(const MatrixRef<double> X, unsigned int alfa) {
+    // Calcular X - mean
+    vector<double> mean = get_mean(*X);
+    MatrixRef<double> X_sub_mean = substract_mean(*X, mean);
+
+    // Calcular M_x
+    X_sub_mean->inplaceTranspose();
+    MatrixRef<double> M_x = X_sub_mean->transposedProduct()->dotProduct(1.0/ (X_sub_mean->width() - 1));
+
+    // Calcular autovalores y autovectores
+    vector<vector<double>> eigenvectors = get_eigenvectors(M_x, std::min(alfa, (unsigned int) X_sub_mean->width()));
+
+    // Construir V ortogonal con los autovectores de M_x dispuestos como
+    // columnas. Alfa es la cantidad de autovectores que se toman; se
+    // descartan el resto de los autovectores, tomando de esta manera
+    // solo los principales.
+    // Como solo usamos V^t, la creamos con los autovectores como filas
+    return FullMatrix<double>::create(eigenvectors);
+}
--- src/pca.h
+++ src/pca.h
@@ -2,68 +2,16 @@
 #define METNUM_TP2_PCA_H
 
 #include <vector>
-#include <limits>
-#include "power.h"
-#include "benchmark/benchmark.h"
 
-#define BENCH_FILE_EIGENVECTORS "bench-eigenvectors.csv"
+vector<double> get_mean(const Matrix<double> &X);
 
-vector<double> get_mean(const Matrix<double> &X) {
-    vector<double> mean;
-    for (size_t j = 0; j < X.width(); j++) {
-        double sum = 0;
-        for (size_t i = 0; i < X.height(); i++)
-            sum += X[i][j];
-        mean.push_back(sum / X.height());
-    }
-    return mean;
-}
+MatrixRef<double> substract_mean(const Matrix<double>& X, vector<double> &mean);
 
-MatrixRef<double> substract_mean(const Matrix<double>& X, vector<double> &mean) {
-    MatrixRef<double> result = X.makeNew();
-    for (size_t i = 0; i < X.height(); i++) {
-        for (size_t j = 0; j < X.width(); j++) {
-            (*result)[i][j] = X[i][j] - mean[j];
-        }
-    }
-    return result;
-}
-
-vector<vector<double>> get_eigenvectors(MatrixRef<double> M_x, unsigned int alfa) {
-    vector<vector<double>> eigenvectors;
-    INIT_BENCH(BENCH_FILE_EIGENVECTORS);
-    for (unsigned int i = 0; i < alfa; i++) {
-        vector<double> eigenvector;
-        START_BENCH;
-        double eigenvalue = power_method(M_x, eigenvector, 0.001, 30);
-        END_BENCH(BENCH_FILE_EIGENVECTORS);
-        eigenvectors.push_back(eigenvector);
-        M_x = deflate(M_x, eigenvector, eigenvalue);
-    }
-    return eigenvectors;
-}
+vector<vector<double>> get_eigenvectors(MatrixRef<double> M_x, unsigned int alfa);
 
 /*
  * 1 <= alfa <= filas(X)
  */
-MatrixRef<double> pca(const MatrixRef<double> X, unsigned int alfa) {
-    // Calcular X - mean
-    vector<double> mean = get_mean(*X);
-    MatrixRef<double> X_sub_mean = substract_mean(*X, mean);
-
-    // Calcular M_x
-    X_sub_mean->inplaceTranspose();
-    MatrixRef<double> M_x = X_sub_mean->transposedProduct()->dotProduct(1.0/ (X_sub_mean->width() - 1));
-
-    // Calcular autovalores y autovectores
-    vector<vector<double>> eigenvectors = get_eigenvectors(M_x, std::min(alfa, (unsigned int) X_sub_mean->width()));
-
-    // Construir V ortogonal con los autovectores de M_x dispuestos como
-    // columnas. Alfa es la cantidad de autovectores que se toman; se
-    // descartan el resto de los autovectores, tomando de esta manera
-    // solo los principales.
-    // Como solo usamos V^t, la creamos con los autovectores como filas
-    return FullMatrix<double>::create(eigenvectors);
-}
+MatrixRef<double> pca(const MatrixRef<double> X, unsigned int alfa = 100);
 
 #endif //METNUM_TP2_PCA_H
